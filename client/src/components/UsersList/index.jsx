1d100daa0814ff59408e4ca7a5b189c5c5dba597
Vasyl Ulianovskyi <vasia.kit434@gmail.com>
2024-10-26 17:50:05 +0300

    Add get users thunck

--- client/src/components/UsersList/index.jsx
+++ client/src/components/UsersList/index.jsx
@@ -1,16 +1,29 @@
+import { useEffect } from 'react';
 import { connect } from 'react-redux';
 import BeatLoader from 'react-spinners/BeatLoader';
 import styles from './UsersList.module.sass';
 import defImage from './defaultPhoto.jpg';
+import { getUsersThunk } from '../../store/slices/usersSlice';
 
-export const UsersList = ({ users, isFetching, error }) => {
+export const UsersList = ({ users, isFetching, error, getUsers }) => {
+  useEffect(() => {
+    getUsers();
+  }, []);
+
   return (
     <>
       <BeatLoader loading={isFetching} />
       {error && <div>!!!ERROR!!!</div>}
       <ul>
         {users.map(u => (
-          <li key={u.id}>{JSON.stringify(u)}</li>
+          <li key={u.id}>
+            <img
+              src={u.image ? `http://localhost:5000/${u.image}` : defImage}
+              alt={u.nickname}
+              className={styles.userImage}
+            />
+            {JSON.stringify(u)}
+          </li>
         ))}
       </ul>
     </>
@@ -19,6 +32,8 @@ export const UsersList = ({ users, isFetching, error }) => {
 
 const mapStateToProps = ({ usersData }) => usersData;
 
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  getUsers: () => dispatch(getUsersThunk()),
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(UsersList);
